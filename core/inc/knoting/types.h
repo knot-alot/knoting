0dc2c5a7ba2e11266efb8aa93eb61a19f95f1df8
Leonardo Mariscal <l@mariscal.ch>
2022-01-26 12:20:15 +0000

    fix: remove dupe submodule, and maths definitions
    Remove `v2`, `v3`, `v4`, `m2`, `m3`, `m4`.
    Rename `v2i` types to `vec2i`
    Remove `bgfx.cmake/` dupe, which is currently `bgfx/`

--- core/inc/knoting/types.h
+++ core/inc/knoting/types.h
@@ -11,35 +11,27 @@ namespace knot {
 
 using namespace glm;
 
-using v2 = vec2;
-using v3 = vec3;
-using v4 = vec4;
-
-using v2i = vec<2, i32>;
-using v2u = vec<2, u32>;
-using v2d = vec<2, f64>;
-using v3i = vec<3, i32>;
-using v3d = vec<3, f64>;
-using v4i = vec<4, i32>;
-using v4d = vec<4, f64>;
-
-using m2 = mat2;
-using m3 = mat3;
-using m4 = mat4;
+using vec2i = vec<2, i32>;
+using vec2u = vec<2, u32>;
+using vec2d = vec<2, f64>;
+using vec3i = vec<3, i32>;
+using vec3d = vec<3, f64>;
+using vec4i = vec<4, i32>;
+using vec4d = vec<4, f64>;
 
 }  // namespace knot
 
-#define IM_VEC2_CLASS_EXTRA     \
-    ImVec2(const knot::v2& f) { \
-        x = f.x;                \
-        y = f.y;                \
-    }                           \
-    operator knot::v2() const { return knot::v2(x, y); }
-#define IM_VEC4_CLASS_EXTRA     \
-    ImVec4(const knot::v4& f) { \
-        x = f.x;                \
-        y = f.y;                \
-        z = f.z;                \
-        w = f.w;                \
-    }                           \
-    operator knot::v4() const { return knot::v4(x, y, z, w); }
+#define IM_VEC2_CLASS_EXTRA       \
+    ImVec2(const knot::vec2& f) { \
+        x = f.x;                  \
+        y = f.y;                  \
+    }                             \
+    operator knot::vec2() const { return knot::vec2(x, y); }
+#define IM_VEC4_CLASS_EXTRA       \
+    ImVec4(const knot::vec4& f) { \
+        x = f.x;                  \
+        y = f.y;                  \
+        z = f.z;                  \
+        w = f.w;                  \
+    }                             \
+    operator knot::vec4() const { return knot::vec4(x, y, z, w); }
