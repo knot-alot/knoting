282dd2d7317db3e4ef5fb3a3ce1cb36b93ebdd42
Leonardo Mariscal <l@mariscal.ch>
2022-02-03 11:56:21 +0000

    feat: add copy resources build stage

--- core/inc/knoting/types.h
+++ core/inc/knoting/types.h
@@ -4,6 +4,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/euler_angles.hpp>
 #include <glm/gtx/quaternion.hpp>
 #include <glm/gtx/string_cast.hpp>
 
