9f8394a166e8fac10b32a11b8007e35e935bf8fa
icewinow <1021695155@qq.com>
2022-02-22 11:25:12 +0000

    clang format

--- core/inc/knoting/engine.h
+++ core/inc/knoting/engine.h
@@ -11,28 +11,28 @@
 #include <knoting/window.h>
 
 namespace knot {
-    class Engine {
-    public:
-        Engine();
-        ~Engine();
+class Engine {
+   public:
+    Engine();
+    ~Engine();
 
-        void update_modules();
-        bool is_open();
+    void update_modules();
+    bool is_open();
 
-        std::weak_ptr<Window> get_window_module() { return m_windowModule; }
-        std::weak_ptr<ForwardRenderer> get_forward_render_module() { return m_forwardRenderModule; }
-        std::shared_ptr<Physics> get_physics_moddule() { return m_physicsModule; }
+    std::weak_ptr<Window> get_window_module() { return m_windowModule; }
+    std::weak_ptr<ForwardRenderer> get_forward_render_module() { return m_forwardRenderModule; }
+    std::shared_ptr<Physics> get_physics_moddule() { return m_physicsModule; }
 
-    private:
-        int m_windowWidth = 1024;
-        int m_windowHeight = 768;
-        std::string m_windowTitle = "hello knotting!";
+   private:
+    int m_windowWidth = 1024;
+    int m_windowHeight = 768;
+    std::string m_windowTitle = "hello knotting!";
 
-    private:
-        std::vector<std::shared_ptr<Subsystem>> m_engineModules;
-        std::shared_ptr<Window> m_windowModule;
-        std::shared_ptr<ForwardRenderer> m_forwardRenderModule;
-        std::shared_ptr<Physics> m_physicsModule;
-    };
+   private:
+    std::vector<std::shared_ptr<Subsystem>> m_engineModules;
+    std::shared_ptr<Window> m_windowModule;
+    std::shared_ptr<ForwardRenderer> m_forwardRenderModule;
+    std::shared_ptr<Physics> m_physicsModule;
+};
 
 }  // namespace knot
