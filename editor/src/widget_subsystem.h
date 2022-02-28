3c98825fd37798744d454e4fcea8a07da7ef0822
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2022-02-28 16:02:06 +0000

    ubuntu / windows / osx  '#defines' for 'glfw3native.h'

--- editor/src/widget_subsystem.h
+++ editor/src/widget_subsystem.h
@@ -1,6 +1,4 @@
 #pragma once
-#define GLFW_EXPOSE_NATIVE_WGL
-#define GLFW_EXPOSE_NATIVE_WIN32
 
 #include <bgfx/bgfx.h>
 #include <imgui.h>
@@ -11,6 +9,17 @@
 #include "knoting/log.h"
 #include "knoting/subsystem.h"
 #include "widget.h"
+
+#if BX_PLATFORM_LINUX
+#define GLFW_EXPOSE_NATIVE_X11
+#elif BX_PLATFORM_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#elif BX_PLATFORM_OSX
+#define GLFW_EXPOSE_NATIVE_COCOA
+#endif
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
