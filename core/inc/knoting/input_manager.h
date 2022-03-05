231d923cb813b6d11530563a2733f1a9c7abed28
Thomas Scott <38812486+thomasscott95@users.noreply.github.com>
2022-03-05 17:45:31 +0000

    reformat

--- core/inc/knoting/input_manager.h
+++ core/inc/knoting/input_manager.h
@@ -3,253 +3,244 @@
 
 struct GLFWwindow;
 
-namespace knot{
+namespace knot {
 
-    class Window;
+class Window;
 
-    enum class MouseButtonCode {
-        Left = 0,
-        Right = 1,
-        Middle = 2,
-        Button3 = 3,
-        Button4 = 4,
-        Button5 = 5,
-        Button6 = 6,
-        Button7 = 7,
-        Button8 = 8,
-        Last
-    };
+enum class MouseButtonCode {
+    Left = 0,
+    Right = 1,
+    Middle = 2,
+    Button3 = 3,
+    Button4 = 4,
+    Button5 = 5,
+    Button6 = 6,
+    Button7 = 7,
+    Button8 = 8,
+    Last
+};
 
-    enum class JoyStickCode {
-        LeftX = 0,
-        LeftY = 1,
-        RightX = 2,
-        RightY = 3,
-        LeftBumper = 4,
-        RightBumper = 5,
-        Last };
+enum class JoyStickCode { LeftX = 0, LeftY = 1, RightX = 2, RightY = 3, LeftBumper = 4, RightBumper = 5, Last };
 
-    enum class PadCode {
-        Pad0 = 0,
-        Pad1 = 1,
-        Pad2 = 2,
-        Pad3 = 3,
-        Pad4 = 4,
-        Pad5 = 5,
-        Pad6 = 6,
-        Pad7 = 7,
-        Pad8 = 8,
-        Pad9 = 9,
-        Pad10 = 10,
-        Pad11 = 11,
-        Pad12 = 12,
-        Pad13 = 13,
-        Pad14 = 14,
-        Pad15 = 15,
-        Last
-    };
+enum class PadCode {
+    Pad0 = 0,
+    Pad1 = 1,
+    Pad2 = 2,
+    Pad3 = 3,
+    Pad4 = 4,
+    Pad5 = 5,
+    Pad6 = 6,
+    Pad7 = 7,
+    Pad8 = 8,
+    Pad9 = 9,
+    Pad10 = 10,
+    Pad11 = 11,
+    Pad12 = 12,
+    Pad13 = 13,
+    Pad14 = 14,
+    Pad15 = 15,
+    Last
+};
 
-    enum class PadButtonCode {
-        A = 0,
-        B = 1,
-        X = 2,
-        Y = 3,
-        LeftBumper = 4,
-        RightBumper = 5,
-        Back = 6,
-        Start = 7,
-        Guide = 8,
-        LeftThumb = 9,
-        RightThumb = 10,
-        DPadUp = 11,
-        DPadRight = 12,
-        DPadDown = 13,
-        DPadLeft = 14,
-        Last
-    };
+enum class PadButtonCode {
+    A = 0,
+    B = 1,
+    X = 2,
+    Y = 3,
+    LeftBumper = 4,
+    RightBumper = 5,
+    Back = 6,
+    Start = 7,
+    Guide = 8,
+    LeftThumb = 9,
+    RightThumb = 10,
+    DPadUp = 11,
+    DPadRight = 12,
+    DPadDown = 13,
+    DPadLeft = 14,
+    Last
+};
 
-    enum class KeyCode {
-        Unknown = -1,
-        Space = 32,
-        Apostrophe = 39,
-        Comma = 44,
-        Minus = 45,
-        Period = 46,
-        Slash = 47,
-        N0 = 48,
-        N1 = 49,
-        N2 = 50,
-        N3 = 51,
-        N4 = 52,
-        N5 = 53,
-        N6 = 54,
-        N7 = 55,
-        N8 = 56,
-        N9 = 57,
-        Semicolon = 59,
-        Equal = 61,
-        A = 65,
-        B = 66,
-        C = 67,
-        D = 68,
-        E = 69,
-        F = 70,
-        G = 71,
-        H = 72,
-        I = 73,
-        J = 74,
-        K = 75,
-        L = 76,
-        M = 77,
-        N = 78,
-        O = 79,
-        P = 80,
-        Q = 81,
-        R = 82,
-        S = 83,
-        T = 84,
-        U = 85,
-        V = 86,
-        W = 87,
-        X = 88,
-        Y = 89,
-        Z = 90,
-        LeftBracket = 91,
-        Backslash = 92,
-        RightBracket = 93,
-        GraveAccent = 96,
-        World1 = 161,
-        World2 = 162,
-        Escape = 256,
-        Enter = 257,
-        Tab = 258,
-        Backspace = 259,
-        Insert = 260,
-        Delete = 261,
-        Right = 262,
-        Left = 263,
-        Down = 264,
-        Up = 265,
-        PageUp = 266,
-        PageDown = 267,
-        Home = 268,
-        End = 269,
-        CapsLock = 280,
-        ScrollLock = 281,
-        NumLock = 282,
-        PrintScreen = 283,
-        Pause = 284,
-        F1 = 290,
-        F2 = 291,
-        F3 = 292,
-        F4 = 293,
-        F5 = 294,
-        F6 = 295,
-        F7 = 296,
-        F8 = 297,
-        F9 = 298,
-        F10 = 299,
-        F11 = 300,
-        F12 = 301,
-        F13 = 302,
-        F14 = 303,
-        F15 = 304,
-        F16 = 305,
-        F17 = 306,
-        F18 = 307,
-        F19 = 308,
-        F20 = 309,
-        F21 = 310,
-        F22 = 311,
-        F23 = 312,
-        F24 = 313,
-        F25 = 314,
-        KP0 = 320,
-        KP1 = 321,
-        KP2 = 322,
-        KP3 = 323,
-        KP4 = 324,
-        KP5 = 325,
-        KP6 = 326,
-        KP7 = 327,
-        KP8 = 328,
-        KP9 = 329,
-        KPDecimal = 330,
-        KPDivide = 331,
-        KPMultiply = 332,
-        KPSubtract = 333,
-        KPAdd = 334,
-        KPEnter = 335,
-        KPEqual = 336,
-        LeftShift = 340,
-        LeftControl = 341,
-        LeftAlt = 342,
-        LeftSuper = 343,
-        RightShift = 344,
-        RightControl = 345,
-        RightAlt = 346,
-        RightSuper = 347,
-        Menu = 348,
-        Last
-    };
+enum class KeyCode {
+    Unknown = -1,
+    Space = 32,
+    Apostrophe = 39,
+    Comma = 44,
+    Minus = 45,
+    Period = 46,
+    Slash = 47,
+    N0 = 48,
+    N1 = 49,
+    N2 = 50,
+    N3 = 51,
+    N4 = 52,
+    N5 = 53,
+    N6 = 54,
+    N7 = 55,
+    N8 = 56,
+    N9 = 57,
+    Semicolon = 59,
+    Equal = 61,
+    A = 65,
+    B = 66,
+    C = 67,
+    D = 68,
+    E = 69,
+    F = 70,
+    G = 71,
+    H = 72,
+    I = 73,
+    J = 74,
+    K = 75,
+    L = 76,
+    M = 77,
+    N = 78,
+    O = 79,
+    P = 80,
+    Q = 81,
+    R = 82,
+    S = 83,
+    T = 84,
+    U = 85,
+    V = 86,
+    W = 87,
+    X = 88,
+    Y = 89,
+    Z = 90,
+    LeftBracket = 91,
+    Backslash = 92,
+    RightBracket = 93,
+    GraveAccent = 96,
+    World1 = 161,
+    World2 = 162,
+    Escape = 256,
+    Enter = 257,
+    Tab = 258,
+    Backspace = 259,
+    Insert = 260,
+    Delete = 261,
+    Right = 262,
+    Left = 263,
+    Down = 264,
+    Up = 265,
+    PageUp = 266,
+    PageDown = 267,
+    Home = 268,
+    End = 269,
+    CapsLock = 280,
+    ScrollLock = 281,
+    NumLock = 282,
+    PrintScreen = 283,
+    Pause = 284,
+    F1 = 290,
+    F2 = 291,
+    F3 = 292,
+    F4 = 293,
+    F5 = 294,
+    F6 = 295,
+    F7 = 296,
+    F8 = 297,
+    F9 = 298,
+    F10 = 299,
+    F11 = 300,
+    F12 = 301,
+    F13 = 302,
+    F14 = 303,
+    F15 = 304,
+    F16 = 305,
+    F17 = 306,
+    F18 = 307,
+    F19 = 308,
+    F20 = 309,
+    F21 = 310,
+    F22 = 311,
+    F23 = 312,
+    F24 = 313,
+    F25 = 314,
+    KP0 = 320,
+    KP1 = 321,
+    KP2 = 322,
+    KP3 = 323,
+    KP4 = 324,
+    KP5 = 325,
+    KP6 = 326,
+    KP7 = 327,
+    KP8 = 328,
+    KP9 = 329,
+    KPDecimal = 330,
+    KPDivide = 331,
+    KPMultiply = 332,
+    KPSubtract = 333,
+    KPAdd = 334,
+    KPEnter = 335,
+    KPEqual = 336,
+    LeftShift = 340,
+    LeftControl = 341,
+    LeftAlt = 342,
+    LeftSuper = 343,
+    RightShift = 344,
+    RightControl = 345,
+    RightAlt = 346,
+    RightSuper = 347,
+    Menu = 348,
+    Last
+};
 
-    class input_manager {
-       public:
-        ///KeyBoard Methods
-        bool m_keyPressed(KeyCode key);
-        bool m_keyHeldDown(KeyCode key);
-        bool m_keyOnTrigger(KeyCode key);
-        bool m_keyOnRelease(KeyCode key);
+class input_manager {
+   public:
+    /// KeyBoard Methods
+    bool m_keyPressed(KeyCode key);
+    bool m_keyHeldDown(KeyCode key);
+    bool m_keyOnTrigger(KeyCode key);
+    bool m_keyOnRelease(KeyCode key);
 
-        ///Pad Methods
-        bool  m_padButtonPressed(PadCode pad, PadButtonCode button);
-        bool  m_padButtonHeldDown(PadCode pad, PadButtonCode button);
-        bool  m_padButtonTriggered(PadCode pad, PadButtonCode button);
-        bool  m_padButtonReleased(PadCode pad, PadButtonCode button);
-        float m_getPadAxis(PadCode pad, JoyStickCode joy_stick);
-        bool  m_padPresent(PadCode pad);
+    /// Pad Methods
+    bool m_padButtonPressed(PadCode pad, PadButtonCode button);
+    bool m_padButtonHeldDown(PadCode pad, PadButtonCode button);
+    bool m_padButtonTriggered(PadCode pad, PadButtonCode button);
+    bool m_padButtonReleased(PadCode pad, PadButtonCode button);
+    float m_getPadAxis(PadCode pad, JoyStickCode joy_stick);
+    bool m_padPresent(PadCode pad);
 
-        ///Mouse Methods
-        bool m_mouseButtonPressed(MouseButtonCode button);
-        bool m_mouseButtonHeldDown(MouseButtonCode button);
-        bool m_mouseButtonTriggered(MouseButtonCode button);
-        bool m_mouseButtonReleased(MouseButtonCode button);
-        bool m_mouseDoubleClicked(MouseButtonCode button);
-        void SetDoubleClickLimit(float ms);
-        void m_setMouseSensitivity(float sensitivity);
-        bool m_wheelMoved();
-        vec2 m_getRelativePosition();
-        vec2 m_getAbsolutePosition();
-        vec2 m_getWheelMovement();
+    /// Mouse Methods
+    bool m_mouseButtonPressed(MouseButtonCode button);
+    bool m_mouseButtonHeldDown(MouseButtonCode button);
+    bool m_mouseButtonTriggered(MouseButtonCode button);
+    bool m_mouseButtonReleased(MouseButtonCode button);
+    bool m_mouseDoubleClicked(MouseButtonCode button);
+    void SetDoubleClickLimit(float ms);
+    void m_setMouseSensitivity(float sensitivity);
+    bool m_wheelMoved();
+    vec2 m_getRelativePosition();
+    vec2 m_getAbsolutePosition();
+    vec2 m_getWheelMovement();
 
+   protected:
+    friend Window;
 
+    input_manager(Window& owner);
+    ~input_manager();
 
-       protected:
-        friend Window;
+    void m_updatePads(GLFWwindow* glfwWindow);
+    void m_keyEvent(int key, bool pressed);
+    void m_mouseButtonEvent(int button, bool pressed);
+    void m_mouseEvent(double x, double y);
+    void m_updateHolds();
+    void m_updateRelativePositions();
+    void m_scrollEvent(vec2 offset);
 
-        input_manager(Window &owner);
-        ~input_manager();
+    bool m_keyBinding[(int)KeyCode::Last];
+    bool m_mouseBindings[(int)MouseButtonCode::Last];
+    bool m_padBindings[(int)PadCode::Last][(int)PadButtonCode::Last];
+    bool m_holdKeyBindings[(int)KeyCode::Last];
+    bool m_holdMouseBindings[(int)MouseButtonCode::Last];
+    bool m_holdPadBindings[(int)PadCode::Last][(int)PadButtonCode::Last];
+    vec2 m_lastScroll;
+    vec<2, double> m_mousePosition;
+    vec<2, double> m_lastMousePosition;
+    vec<2, double> m_relativeMousePosition;
+    float m_joyStickBindings[(int)PadCode::Last][(int)JoyStickCode::Last];
+    float m_sensitivity;
+};
 
-        void m_updatePads(GLFWwindow *glfwWindow);
-        void m_keyEvent(int key, bool pressed);
-        void m_mouseButtonEvent(int button, bool pressed);
-        void m_mouseEvent(double x, double y);
-        void m_updateHolds();
-        void m_updateRelativePositions();
-        void m_scrollEvent(vec2 offset);
-
-        bool m_keyBinding[(int)KeyCode::Last];
-        bool m_mouseBindings[(int)MouseButtonCode::Last];
-        bool m_padBindings[(int)PadCode::Last][(int)PadButtonCode::Last];
-        bool m_holdKeyBindings[(int)KeyCode::Last];
-        bool m_holdMouseBindings[(int)MouseButtonCode::Last];
-        bool m_holdPadBindings[(int)PadCode::Last][(int)PadButtonCode::Last];
-        vec2 m_lastScroll;
-        vec<2,double> m_mousePosition;
-        vec<2,double> m_lastMousePosition;
-        vec<2,double> m_relativeMousePosition;
-        float m_joyStickBindings[(int)PadCode::Last][(int)JoyStickCode::Last];
-        float m_sensitivity;
-    };
-
-} // namespace knot
+}  // namespace knot
--- core/src/input_manager.cpp
+++ core/src/input_manager.cpp
@@ -1,187 +1,205 @@
+#include <GLFW/glfw3.h>
 #include <knoting/input_manager.h>
 #include <knoting/window.h>
 #include <algorithm>
-#include <GLFW/glfw3.h>
 
-namespace knot{
+namespace knot {
 
-    input_manager::input_manager(Window &owner): m_sensitivity(0.3f),m_lastScroll(0.0f) {
-        for(int i = 0; i < (int)KeyCode::Last; ++i){
-            m_keyBinding[i] = false;
+input_manager::input_manager(Window& owner) : m_sensitivity(0.3f), m_lastScroll(0.0f) {
+    for (int i = 0; i < (int)KeyCode::Last; ++i) {
+        m_keyBinding[i] = false;
+        m_holdKeyBindings[i] = false;
+    }
+
+    for (int i = 0; i < (int)MouseButtonCode::Last; ++i) {
+        m_mouseBindings[i] = false;
+        m_holdMouseBindings[i] = false;
+    }
+
+    for (int i = 0; i < (int)PadCode::Last; ++i) {
+        for (int j = 0; j < (int)PadButtonCode::Last; ++j) {
+            m_padBindings[i][j] = false;
             m_holdKeyBindings[i] = false;
         }
 
-        for(int i = 0; i < (int)MouseButtonCode::Last; ++i){
-            m_mouseBindings[i] = false;
-            m_holdMouseBindings[i] = false;
+        for (int j = 0; j < (int)JoyStickCode::Last; ++j) {
+            m_joyStickBindings[i][j] = 0.0f;
         }
-
-        for(int i = 0; i < (int)PadCode::Last; ++i) {
-            for (int j = 0; j < (int)PadButtonCode::Last; ++j) {
-                m_padBindings[i][j] = false;
-                m_holdKeyBindings[i] = false;
-            }
-
-            for (int j = 0; j < (int)JoyStickCode::Last; ++j) {
-                m_joyStickBindings[i][j] = 0.0f;
-            }
-        }
-    }
-
-    input_manager::~input_manager() {}
-
-    ///Start Key Keyboard
-    [[maybe_unused]] bool input_manager::m_keyPressed(KeyCode key) {
-        return m_keyBinding[(int)key];
-    }
-
-    bool input_manager::m_keyHeldDown(KeyCode key){
-        return m_keyBinding[(int)key] && m_holdKeyBindings[(int)key];
-    }
-
-    bool input_manager::m_keyOnTrigger(KeyCode key){
-        return m_keyBinding[(int)key] && !m_holdKeyBindings[(int)key];
-    }
-
-    bool input_manager::m_keyOnRelease(KeyCode key){
-        return m_keyBinding[(int)key] && !m_holdKeyBindings[(int)key];
-    }
-    ///End Of Keyboard
-
-    ///Start Of Pad
-    bool input_manager::m_padPresent(PadCode pad) {
-        return glfwJoystickPresent((int)pad);
-    }
-
-    bool input_manager::m_padButtonPressed(PadCode pad, PadButtonCode button) {
-        return m_padBindings[(int)pad][(int)button];
-    }
-
-    bool input_manager::m_padButtonHeldDown(PadCode pad, PadButtonCode button) {
-        return m_padBindings[(int)pad][(int)button] && m_holdPadBindings[(int)pad][(int)button];
-    }
-
-    bool input_manager::m_padButtonTriggered(PadCode pad, PadButtonCode button) {
-        return m_padBindings[(int)pad][(int)button] && !m_holdPadBindings[(int)pad][(int)button];
-    }
-
-    bool input_manager::m_padButtonReleased(PadCode pad, PadButtonCode button) {
-        return m_padBindings[(int)pad][(int)button] && !m_holdPadBindings[(int)pad][(int)button];
-    }
-
-    float input_manager::m_getPadAxis(PadCode pad, JoyStickCode joy_stick) {
-        return m_joyStickBindings[(int)pad][(int)joy_stick];
-    }
-
-
-    void input_manager::m_updatePads(GLFWwindow* glfwWindow) {
-        for (int i = 0; i < (int)PadCode::Last; i++){
-            if(!m_padPresent((PadCode)i)) continue;
-
-            int count;
-            const float *axis = glfwGetJoystickAxes(i, &count);
-            if(!axis) continue;
-
-            if(count>(int)JoyStickCode::Last) count=(int)JoyStickCode::Last;
-            for(int n = 0; n<count; ++n) m_joyStickBindings[i][n]= axis[n];
-
-            GLFWgamepadstate gameState;
-            if(glfwGetGamepadState(i, &gameState)){
-                for(int j = 0; j < (int)PadButtonCode::Last; j++) {
-                    bool padPressed = gameState.buttons[j];
-                    m_padBindings[i][j] = padPressed;
-                }
-            } else if(glfwJoystickPresent(i)) {
-                const unsigned char *buttonPressed = glfwGetJoystickButtons(i, &count);
-                if(!buttonPressed) continue;
-                for (int j = 0; j < count; ++j){
-                    if(count>0)m_padBindings[i][(int)PadButtonCode::A] = buttonPressed[0];
-                    if(count>1)m_padBindings[i][(int)PadButtonCode::B] = buttonPressed[1];
-                    if(count>3)m_padBindings[i][(int)PadButtonCode::X] = buttonPressed[3];
-                    if(count>4)m_padBindings[i][(int)PadButtonCode::Y] = buttonPressed[4];
-                    if(count>6)m_padBindings[i][(int)PadButtonCode::LeftBumper] = buttonPressed[6];
-                    if(count>7)m_padBindings[i][(int)PadButtonCode::RightBumper] = buttonPressed[7];
-                    if(count>11)m_padBindings[i][(int)PadButtonCode::Start] = buttonPressed[11];
-                    if(count>13)m_padBindings[i][(int)PadButtonCode::LeftThumb] = buttonPressed[13];
-                    if(count>14)m_padBindings[i][(int)PadButtonCode::RightThumb] = buttonPressed[14];
-                    if(count>15)m_padBindings[i][(int)PadButtonCode::DPadUp] = buttonPressed[15];
-                    if(count>16)m_padBindings[i][(int)PadButtonCode::DPadRight] = buttonPressed[16];
-                    if(count>17)m_padBindings[i][(int)PadButtonCode::DPadDown] = buttonPressed[17];
-                    if(count>18)m_padBindings[i][(int)PadButtonCode::DPadLeft] = buttonPressed[18];
-                }
-            }
-        }
-    }
-    ///End Of Pad
-
-    ///Start Of Mouse
-    bool input_manager::m_mouseButtonPressed(MouseButtonCode button){
-        return m_mouseBindings[(int)button];
-    }
-
-    [[maybe_unused]] bool input_manager::m_mouseButtonHeldDown(MouseButtonCode button) {
-        return m_mouseBindings[(int)button] && m_holdMouseBindings[(int)button];
-    }
-
-    bool input_manager::m_mouseButtonTriggered(MouseButtonCode button) {
-        return m_mouseBindings[(int)button] && !m_holdMouseBindings[(int)button];
-    }
-
-    bool input_manager::m_mouseButtonReleased(MouseButtonCode button) {
-        return !m_mouseBindings[(int)button] && m_holdMouseBindings[(int)button];
-    }
-
-    bool input_manager::m_mouseDoubleClicked(MouseButtonCode button) {
-        return false;
-    }
-
-    bool input_manager::m_wheelMoved() {
-        return m_lastScroll != vec2 {};
-    }
-
-    vec2 input_manager::m_getRelativePosition() {
-        return vec2(m_relativeMousePosition.x * m_sensitivity, m_relativeMousePosition.y * m_sensitivity);
-    }
-
-    vec2 input_manager::m_getAbsolutePosition() {
-        return vec2(m_mousePosition.x, m_mousePosition.y);
-    }
-
-    [[maybe_unused]] vec2 input_manager::m_getWheelMovement() {
-        return m_lastScroll;
-    }
-
-    void input_manager::m_setMouseSensitivity(float sens) {
-        m_sensitivity = sens;
-    }
-
-    void input_manager::m_updateRelativePositions() {
-        m_relativeMousePosition = m_mousePosition - m_lastMousePosition;
-        m_lastMousePosition = m_mousePosition;
-    }
-    ///End of Mouse
-
-    ///Start of Events
-    void input_manager::m_scrollEvent(vec2 offset) {
-        m_lastScroll = offset;
-    }
-
-    void input_manager::m_keyEvent(int key, bool pressed) {
-        m_keyBinding[key] = pressed;
-    }
-    void input_manager::m_mouseButtonEvent(int button, bool pressed) {
-        m_mouseBindings[button] = pressed;
-    }
-    void input_manager::m_mouseEvent(double x, double y) {
-        m_mousePosition.x = x;
-        m_mousePosition.y = y;
-    }
-    ///End of Events
-
-    void input_manager::m_updateHolds(){
-        std::copy_n(m_keyBinding,(int)KeyCode::Last,m_holdKeyBindings);
-        std::copy_n(m_mouseBindings,(int)MouseButtonCode::Last,m_holdMouseBindings);
-        for(int i=0; i<(int)PadCode::Last; ++i) std::copy_n(m_padBindings,(int)PadCode::Last,m_holdMouseBindings);
-        m_lastScroll = vec2(0.0f);
     }
 }
+
+input_manager::~input_manager() {}
+
+/// Start Key Keyboard
+[[maybe_unused]] bool input_manager::m_keyPressed(KeyCode key) {
+    return m_keyBinding[(int)key];
+}
+
+bool input_manager::m_keyHeldDown(KeyCode key) {
+    return m_keyBinding[(int)key] && m_holdKeyBindings[(int)key];
+}
+
+bool input_manager::m_keyOnTrigger(KeyCode key) {
+    return m_keyBinding[(int)key] && !m_holdKeyBindings[(int)key];
+}
+
+bool input_manager::m_keyOnRelease(KeyCode key) {
+    return m_keyBinding[(int)key] && !m_holdKeyBindings[(int)key];
+}
+/// End Of Keyboard
+
+/// Start Of Pad
+bool input_manager::m_padPresent(PadCode pad) {
+    return glfwJoystickPresent((int)pad);
+}
+
+bool input_manager::m_padButtonPressed(PadCode pad, PadButtonCode button) {
+    return m_padBindings[(int)pad][(int)button];
+}
+
+bool input_manager::m_padButtonHeldDown(PadCode pad, PadButtonCode button) {
+    return m_padBindings[(int)pad][(int)button] && m_holdPadBindings[(int)pad][(int)button];
+}
+
+bool input_manager::m_padButtonTriggered(PadCode pad, PadButtonCode button) {
+    return m_padBindings[(int)pad][(int)button] && !m_holdPadBindings[(int)pad][(int)button];
+}
+
+bool input_manager::m_padButtonReleased(PadCode pad, PadButtonCode button) {
+    return m_padBindings[(int)pad][(int)button] && !m_holdPadBindings[(int)pad][(int)button];
+}
+
+float input_manager::m_getPadAxis(PadCode pad, JoyStickCode joy_stick) {
+    return m_joyStickBindings[(int)pad][(int)joy_stick];
+}
+
+void input_manager::m_updatePads(GLFWwindow* glfwWindow) {
+    for (int i = 0; i < (int)PadCode::Last; i++) {
+        if (!m_padPresent((PadCode)i))
+            continue;
+
+        int count;
+        const float* axis = glfwGetJoystickAxes(i, &count);
+        if (!axis)
+            continue;
+
+        if (count > (int)JoyStickCode::Last)
+            count = (int)JoyStickCode::Last;
+        for (int n = 0; n < count; ++n)
+            m_joyStickBindings[i][n] = axis[n];
+
+        GLFWgamepadstate gameState;
+        if (glfwGetGamepadState(i, &gameState)) {
+            for (int j = 0; j < (int)PadButtonCode::Last; j++) {
+                bool padPressed = gameState.buttons[j];
+                m_padBindings[i][j] = padPressed;
+            }
+        } else if (glfwJoystickPresent(i)) {
+            const unsigned char* buttonPressed = glfwGetJoystickButtons(i, &count);
+            if (!buttonPressed)
+                continue;
+            for (int j = 0; j < count; ++j) {
+                if (count > 0)
+                    m_padBindings[i][(int)PadButtonCode::A] = buttonPressed[0];
+                if (count > 1)
+                    m_padBindings[i][(int)PadButtonCode::B] = buttonPressed[1];
+                if (count > 3)
+                    m_padBindings[i][(int)PadButtonCode::X] = buttonPressed[3];
+                if (count > 4)
+                    m_padBindings[i][(int)PadButtonCode::Y] = buttonPressed[4];
+                if (count > 6)
+                    m_padBindings[i][(int)PadButtonCode::LeftBumper] = buttonPressed[6];
+                if (count > 7)
+                    m_padBindings[i][(int)PadButtonCode::RightBumper] = buttonPressed[7];
+                if (count > 11)
+                    m_padBindings[i][(int)PadButtonCode::Start] = buttonPressed[11];
+                if (count > 13)
+                    m_padBindings[i][(int)PadButtonCode::LeftThumb] = buttonPressed[13];
+                if (count > 14)
+                    m_padBindings[i][(int)PadButtonCode::RightThumb] = buttonPressed[14];
+                if (count > 15)
+                    m_padBindings[i][(int)PadButtonCode::DPadUp] = buttonPressed[15];
+                if (count > 16)
+                    m_padBindings[i][(int)PadButtonCode::DPadRight] = buttonPressed[16];
+                if (count > 17)
+                    m_padBindings[i][(int)PadButtonCode::DPadDown] = buttonPressed[17];
+                if (count > 18)
+                    m_padBindings[i][(int)PadButtonCode::DPadLeft] = buttonPressed[18];
+            }
+        }
+    }
+}
+/// End Of Pad
+
+/// Start Of Mouse
+bool input_manager::m_mouseButtonPressed(MouseButtonCode button) {
+    return m_mouseBindings[(int)button];
+}
+
+[[maybe_unused]] bool input_manager::m_mouseButtonHeldDown(MouseButtonCode button) {
+    return m_mouseBindings[(int)button] && m_holdMouseBindings[(int)button];
+}
+
+bool input_manager::m_mouseButtonTriggered(MouseButtonCode button) {
+    return m_mouseBindings[(int)button] && !m_holdMouseBindings[(int)button];
+}
+
+bool input_manager::m_mouseButtonReleased(MouseButtonCode button) {
+    return !m_mouseBindings[(int)button] && m_holdMouseBindings[(int)button];
+}
+
+bool input_manager::m_mouseDoubleClicked(MouseButtonCode button) {
+    return false;
+}
+
+bool input_manager::m_wheelMoved() {
+    return m_lastScroll != vec2{};
+}
+
+vec2 input_manager::m_getRelativePosition() {
+    return vec2(m_relativeMousePosition.x * m_sensitivity, m_relativeMousePosition.y * m_sensitivity);
+}
+
+vec2 input_manager::m_getAbsolutePosition() {
+    return vec2(m_mousePosition.x, m_mousePosition.y);
+}
+
+[[maybe_unused]] vec2 input_manager::m_getWheelMovement() {
+    return m_lastScroll;
+}
+
+void input_manager::m_setMouseSensitivity(float sens) {
+    m_sensitivity = sens;
+}
+
+void input_manager::m_updateRelativePositions() {
+    m_relativeMousePosition = m_mousePosition - m_lastMousePosition;
+    m_lastMousePosition = m_mousePosition;
+}
+/// End of Mouse
+
+/// Start of Events
+void input_manager::m_scrollEvent(vec2 offset) {
+    m_lastScroll = offset;
+}
+
+void input_manager::m_keyEvent(int key, bool pressed) {
+    m_keyBinding[key] = pressed;
+}
+void input_manager::m_mouseButtonEvent(int button, bool pressed) {
+    m_mouseBindings[button] = pressed;
+}
+void input_manager::m_mouseEvent(double x, double y) {
+    m_mousePosition.x = x;
+    m_mousePosition.y = y;
+}
+/// End of Events
+
+void input_manager::m_updateHolds() {
+    std::copy_n(m_keyBinding, (int)KeyCode::Last, m_holdKeyBindings);
+    std::copy_n(m_mouseBindings, (int)MouseButtonCode::Last, m_holdMouseBindings);
+    for (int i = 0; i < (int)PadCode::Last; ++i)
+        std::copy_n(m_padBindings, (int)PadCode::Last, m_holdMouseBindings);
+    m_lastScroll = vec2(0.0f);
+}
+}  // namespace knot
