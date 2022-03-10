0b813b01f4cc0e02b7b02fd0abc7f91485bbf7c0
Bert_T <bertie.layout@gmail.com>
2022-03-10 20:02:28 +0000

    clang-format-fix

--- core/inc/knoting/game_object.h
+++ core/inc/knoting/game_object.h
@@ -227,7 +227,7 @@ class Tag {
         archive(this->get_registered_tags());
     }
 
-    void set_tag(uint16_t id) { m_id  =id;}
+    void set_tag(uint16_t id) { m_id = id; }
 
     static uint16_t get_id_from_tag(const std::string& tag);
     static std::string get_tag_from_id(uint16_t id);
