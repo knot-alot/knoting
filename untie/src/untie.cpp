e09d48db45c014f25211c4b1a0b9b6922f598189
Bert_T <bertie.layout@gmail.com>
2022-03-16 15:13:06 +0000

    clear-up-scene

--- untie/src/untie.cpp
+++ untie/src/untie.cpp
@@ -83,14 +83,14 @@ Untie::Untie() {
     {
         auto cubeObj = scene.create_game_object("floor");
         cubeObj.get_component<components::Transform>().set_position(glm::vec3(-5.0f, 0.0f, -10.0f));
-        cubeObj.get_component<components::Transform>().set_scale(glm::vec3(15, 1, 15));
+        cubeObj.get_component<components::Transform>().set_scale(glm::vec3(25, 1, 25));
         cubeObj.get_component<components::Transform>().set_rotation_euler(glm::vec3(0, 45, 0));
         cubeObj.add_component<components::InstanceMesh>("uv_cube.obj");
 
         auto& physics_material = cubeObj.add_component<components::PhysicsMaterial>();
 
         auto& shape = cubeObj.add_component<components::Shape>();
-        vec3 halfsize = vec3(15.0, 1.0f, 15.0);
+        vec3 halfsize = vec3(25.0, 1.0f, 25.0);
         shape.set_geometry(shape.create_cube_geometry(halfsize));
 
         auto& rigidbody = cubeObj.add_component<components::RigidBody>();
@@ -104,61 +104,6 @@ Untie::Untie() {
         cubeObj.add_component<components::Material>(material);
 
         rigidbody.create_actor(false);
-    }
-
-    {
-        auto cubeObj = scene.create_game_object("cube_1");
-        cubeObj.get_component<components::Transform>().set_position(glm::vec3(-5.0f, 5.0f, -10.0f));
-        cubeObj.add_component<components::InstanceMesh>("uv_cube.obj");
-        auto& physics_material = cubeObj.add_component<components::PhysicsMaterial>();
-
-        auto& shape = cubeObj.add_component<components::Shape>();
-        vec3 halfsize = vec3(1.0f, 1.0f, 1.0f);
-        shape.set_geometry(shape.create_cube_geometry(halfsize));
-
-        auto& rigidbody = cubeObj.add_component<components::RigidBody>();
-
-        rigidbody.create_actor(true, 5.0f);
-
-        auto material = components::Material();
-        material.set_texture_slot_path(TextureType::Albedo, "UV_Grid_test.png");
-        material.set_texture_slot_path(TextureType::Normal, "normal_tiles_1k.png");
-        material.set_texture_slot_path(TextureType::Metallic, "whiteTexture");
-        material.set_texture_slot_path(TextureType::Roughness, "whiteTexture");
-        material.set_texture_slot_path(TextureType::Occlusion, "whiteTexture");
-        cubeObj.add_component<components::Material>(material);
-
-        cubeOne = cubeObj;
-    }
-    {
-        auto cubeObj = scene.create_game_object("cube_2");
-        cubeObj.get_component<components::Transform>().set_position(glm::vec3(-4.0f, 7.0f, -9.0f));
-        cubeObj.add_component<components::InstanceMesh>("uv_cube.obj");
-
-        // auto& physics_material = cubeObj.add_component<components::PhysicsMaterial>();
-
-        // auto& shape = cubeObj.add_component<components::Shape>();
-        // vec3 halfsize = vec3(1.0f);
-        // shape.set_geometry(shape.create_cube_geometry(halfsize));
-
-        // auto& rigidbody = cubeObj.add_component<components::RigidBody>();
-        // rigidbody.create_actor(true, 5.0f);
-
-        auto material = components::Material();
-        material.set_texture_slot_path(TextureType::Albedo, "UV_Grid_test.png");
-        material.set_texture_slot_path(TextureType::Normal, "normal_tiles_1k.png");
-        material.set_texture_slot_path(TextureType::Metallic, "whiteTexture");
-        material.set_texture_slot_path(TextureType::Roughness, "whiteTexture");
-        material.set_texture_slot_path(TextureType::Occlusion, "whiteTexture");
-        cubeObj.add_component<components::Material>(material);
-
-        // components::Hierarchy& cubeHierarchy = cubeOne.value().get_component<components::Hierarchy>();
-        // cubeHierarchy.add_child(cubeObj);
-
-        components::Hierarchy& cubeTwoHierarchy = cubeObj.get_component<components::Hierarchy>();
-        cubeTwoHierarchy.set_parent(cubeOne.value());
-
-        cubeTwo = cubeObj;
     }
     {
         auto cubeObj = scene.create_game_object("stanford_dragon_1");
