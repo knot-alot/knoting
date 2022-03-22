d6ffdb089067fe14c8c15fe5d886496de45e7ead
Thomas Scott <38812486+thomasscott95@users.noreply.github.com>
2022-03-22 19:49:37 +0000

    player movement written

--- res/misc/player_movement.ts
+++ res/misc/player_movement.ts
@@ -18,7 +18,7 @@ export default class Player_movement extends GameObject {
     awake() {
     }
 
-    update() {
+    update(deltaTime: number) {
         this.playerInputs();
     }
 
@@ -68,6 +68,39 @@ export default class Player_movement extends GameObject {
         let transform: Transform = this.getComponent("transform");
         let rigidBody: RigidBody = this.getComponent("rigidBody");
 
-        
+        //Camera Rotation X
+        let player_look: Vec2 = clientPlayer.GetLookAxis();
+        let look_x: number = player_look[0];
+
+        let rotDeg: Vec3 = [0, 0, 0];
+        let lookMulti: number = 3;
+        if (look_x > 0) {
+            rotDeg[1] = -1 * lookMulti;
+        } else if (look_x < 0) {
+            rotDeg[1] = 1 * lookMulti;
+        }
+
+        let playerRot: Vec3 = transform.getRotationEuler();
+        let newRot: Vec3 = math.add(playerRot, rotDeg);
+        transform.setRotation(newRot);
+        rigidBody.setRotation(transform.getRotation());
+
+        let moveAxis: Vec2 = clientPlayer.GetMoveAxis();
+        let playerForward: Vec3 = transform.getForward();
+        playerForward = math.multiply([moveAxis[1], moveAxis[1], moveAxis[1]], playerForward);
+
+        let playerRight: Vec3 = transform.getRight();
+        playerRight = math.multiply([moveAxis[0], moveAxis[0], moveAxis[0]], playerRight);
+
+        let playerDir: Vec3 = math.add(playerForward, playerRight);
+
+        playerDir = math._normalize(playerDir);
+
+        let baseMulti: number = 1000;
+        let moveMulti: number = 2;
+
+        baseMulti = baseMulti * moveMulti;
+
+        rigidBody.addForce(math.multiply([baseMulti, baseMulti, baseMulti], playerDir));
     }
 }
